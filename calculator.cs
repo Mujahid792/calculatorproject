568fadbee1f9ff012031dc5b99d2aaae6d63b0e5
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-03-06 09:37:47 +0500

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -5,5 +5,12 @@ class Calculator
     static void Main(string[] args)
     {
         Console.WriteLine("Name: Your Name, Roll No: Your Roll Number");
+
+        // Multiplication
+        Console.WriteLine("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Product: " + (a * b));
     }
 }
