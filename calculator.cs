9c2b9795115f0d8c6e53583dc31328324679ec4a
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-03-06 09:39:16 +0500

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -5,5 +5,15 @@ class Calculator
     static void Main(string[] args)
     {
         Console.WriteLine("Name: Your Name, Roll No: Your Roll Number");
+
+        // Division
+        Console.WriteLine("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        if (b == 0)
+            Console.WriteLine("Cannot divide by zero.");
+        else
+            Console.WriteLine("Quotient: " + (a / b));
     }
 }
