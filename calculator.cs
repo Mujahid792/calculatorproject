e2fef9b527cfcdcc034113bd158bd05211cc97db
Mujahid792 <muhammadmujahidali2@gmail.com>
2025-03-06 09:36:43 +0500

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -5,5 +5,12 @@ class Calculator
     static void Main(string[] args)
     {
         Console.WriteLine("Name: Your Name, Roll No: Your Roll Number");
+
+        // Subtraction
+        Console.WriteLine("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Difference: " + (a - b));
     }
 }
